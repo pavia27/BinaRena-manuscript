64b469b27cdc4b337ab2796b8a287158069b46b8
pavia27 <pavia27@gmail.com>
2022-06-27 12:55:25 -0700

    Update MAQ_tsv_build.R

--- MAQ_tsv_build.R
+++ MAQ_tsv_build.R
@@ -6,47 +6,38 @@ setwd("/Users/michael_pavia/BinaRena")
 #dastool
 a<-read.delim("dastool.map",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header = F,col.names=c("contig","dastool"))
 #GC
-b<-read.delim("GC_contigs.txt",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header = F,col.names=c("contig","GC"))
-b<-separate(b,contig,c("contig","delete"),sep = " ", extra = "merge")
+b<-read.delim("GC_contigs.txt",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header = F,col.names=c("contig","GC")) %>% 
+  separate(contig,c("contig","delete"),sep = " ", extra = "merge") %>%
+  select(-c("delete"))
 #maxbin
 h<-read.delim("maxbin.map",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header = F,col.names=c("contig","maxbin"))
 #metabat
 i<-read.delim("metabat.map",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header = F,col.names=c("contig","metabat"))
-#kraken taxa
-j<-read.delim("rank_names.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(j)<-c("contig","domain","phylum","class","order","family","genus","species")
+#taxa
+j<-read.delim("rank_names.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","domain","phylum","class","order","family","genus","species"))
 #depth
-k<-read.delim("MAQ_depth.abund",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = " ",header =T)
-k<-k[,-2]
-colnames(k)<-c("contig","MAQ_000_10_MG","MAQ_000_20_MG","MAQ_050_10_MG","MAQ_050_20_MG","MAQ_100_10_MG","MAQ_100_20_MG")
-k<-semi_join(k,b)#keep only observations in df1 that match in df2.
+k<-read.delim("MAQ_depth.abund",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = " ",header =T) %>%
+  select(-c(2)) %>%
+  `colnames<-`(c("contig","MAQ_000_10_MG","MAQ_000_20_MG","MAQ_050_10_MG","MAQ_050_20_MG","MAQ_100_10_MG","MAQ_100_20_MG"))
+k<-semi_join(k,b)
 #kmer 4
-o<-read.delim("kmer4.pca.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(o)<-c("contig","PC1_k4","PC2_k4","PC3_k4","PC4_k4","PC5_k4")
-p<-read.delim("kmer4.tsne.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(p)<-c("contig","tSNE1_k4","tSNE2_k4")
-q<-read.delim("kmer4.umap.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(q)<-c("contig","UMAP1_k4","UMAP2_k4")
+o<-read.delim("kmer4.pca.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","PC1_k4","PC2_k4","PC3_k4","PC4_k4","PC5_k4"))
+p<-read.delim("kmer4.tsne.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","tSNE1_k4","tSNE2_k4"))
+q<-read.delim("kmer4.umap.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","UMAP1_k4","UMAP2_k4"))
 o<-o%>%
   full_join(p)%>%
   full_join(q)
 #kmer 5
-r<-read.delim("kmer5.pca.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(r)<-c("contig","PC1_k5","PC2_k5","PC3_k5","PC4_k5","PC5_k5")
-s<-read.delim("kmer5.tsne.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(s)<-c("contig","tSNE1_k5","tSNE2_k5")
-t<-read.delim("kmer5.umap.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(t)<-c("contig","UMAP1_k5","UMAP2_k5")
+r<-read.delim("kmer5.pca.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","PC1_k5","PC2_k5","PC3_k5","PC4_k5","PC5_k5"))
+s<-read.delim("kmer5.tsne.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","tSNE1_k5","tSNE2_k5"))
+t<-read.delim("kmer5.umap.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","UMAP1_k5","UMAP2_k5"))
 r<-r %>%
   full_join(s) %>%
   full_join(t)
 #kmer 6
-u<-read.delim("kmer6.pca.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(u)<-c("contig","PC1_k6","PC2_k6","PC3_k6","PC4_k6","PC5_k6")
-v<-read.delim("kmer6.tsne.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(v)<-c("contig","tSNE1_k6","tSNE2_k6")
-w<-read.delim("kmer6.umap.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T)
-colnames(w)<-c("contig","UMAP1_k6","UMAP2_k6")
+u<-read.delim("kmer6.pca.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","PC1_k6","PC2_k6","PC3_k6","PC4_k6","PC5_k6"))
+v<-read.delim("kmer6.tsne.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","tSNE1_k6","tSNE2_k6"))
+w<-read.delim("kmer6.umap.tsv",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header =T,col.names=c("contig","UMAP1_k6","UMAP2_k6"))
 u<-u %>%
   full_join(v) %>%
   full_join(w)
@@ -86,7 +77,7 @@ z<-c%>%
   full_join(e)
 #add in nitrogen gene potential
 a<-read.delim("ko00910.lst",quote = "",row.names = NULL,stringsAsFactors = FALSE,sep = "\t",header = T)
-b<-read.csv("../MAQ_koscan_clean.csv")
+b<-read.csv("MAQ_koscan_clean.csv")
 c<-semi_join(b,a) %>% 
   separate(gene.name,c("k127","contig_r","drop"),sep = "_", extra = "drop") %>% 
   mutate(contig=paste0(k127,"_",contig_r)) %>%
